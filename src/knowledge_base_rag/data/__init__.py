cc31de86fcb25a288892a18758a3de019f1a1071
PaulWask <pawel.waskiewicz98@gmail.com>
2025-12-29 06:24:17 +0100

    feat: document processor - SentenceSplitter chunking with overlap

--- src/knowledge_base_rag/data/__init__.py
+++ src/knowledge_base_rag/data/__init__.py
@@ -1,5 +1,6 @@
 """Data loading and processing modules."""
 
 from knowledge_base_rag.data.loader import DocumentLoader
+from knowledge_base_rag.data.processor import DocumentProcessor
 
-__all__ = ["DocumentLoader"]
+__all__ = ["DocumentLoader", "DocumentProcessor"]
